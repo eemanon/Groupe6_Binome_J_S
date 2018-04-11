bde36da9b6e63261dfa88169ab719534bcb18436
eemanon <mrklgrnd@yahoo.com>
2018-04-11 16:24:04 +0200

    deleted unnecessary files

--- server/jsontest.py
+++
@@ -1,6 +0,0 @@
-import json
-jsonData = '{"name": "Frank", "age": 39}'
-jsonToPython = json.loads(jsonData)
-print(jsonToPython)
-dictionaryToJson = json.dumps(jsonToPython)
-print(dictionaryToJson)
